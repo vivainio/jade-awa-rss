3d5fff14954b67f8a4af7f117a2675bebc6af426
Ville M. Vainio <vivainio@gmail.com>
2013-02-01 12:35:10 +0200

    add "basic web app example" to assets

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+window.require.define({"index": function(exports, require, module) {
+  module.exports = function anonymous(locals, attrs, escape, rethrow, merge) {
+  attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
+  var buf = [];
+  with (locals || {}) {
+  var interp;
+  buf.push('<p>Lorem ipsum</p>');
+  }
+  return buf.join("");
+  };
+}});
+
